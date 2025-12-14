6d0a7d561fa33a851a9e5aa37bf788e8a8dd1ccb
Milton <gratno@gmail.com>
2025-12-14 16:08:24 -0500

    Perfiles

--- colombia_trends/Colombia_Trends/accounts/models.py
+++ colombia_trends/Colombia_Trends/accounts/models.py
@@ -5,4 +5,12 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User,)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', verbose_name='Usuario')
+    apodo = models.CharField(max_length=150, null=True, blank=True, verbose_name='Apodo')
+    image = models.ImageField(default='users/usuario_defecto.jpg', upload_to='users/', verbose_name='Imagen de perfil')
+    location = models.CharField(max_length=150, null=True, blank=True, verbose_name='Localidad')
+    
+    class Meta:
+        verbo_name = 'perfil'
+        verbo_name = 'perfiles'
+        
