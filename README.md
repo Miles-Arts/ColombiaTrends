5830ffc465540c16b2dbdf6b55c065749975100f
Milton <gratno@gmail.com>
2025-12-15 16:27:26 -0500

    Text

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ source ../env/Scripts/activate
 
 Activar SERVER
 python manage.py runserver
+
+Migrar y activar 
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
--- colombia_trends/Colombia_Trends/accounts/signals.py
+++ colombia_trends/Colombia_Trends/accounts/signals.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.models import Group
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-
 from .models import Profile
 
 @receiver(post_save, sender=Profile)
-def add_user_clientes_group(sender, instance, created, **kwargs):
+def add_user_group(sender, instance, created, **kwargs):
     if created:
         clientes, _ = Group.objects.get_or_create(name='cliente')
-        instance.user.groups.add(clientes)
+        admins, _ = Group.objects.get_or_create(name='admin')
+        # Ejemplo: asignar por username
+        if instance.user.username == 'admin':
+            instance.user.groups.add(admins)
+        else:
+            instance.user.groups.add(clientes)
